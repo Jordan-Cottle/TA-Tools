abf3d53a7d8a79641e45d266567fda36f8a8bdbf
JordanCottle <jordancottle622@gmail.com>
2019-08-27 10:20:35 -0400

    remove commented out code

--- grading.py
+++ grading.py
@@ -34,35 +34,3 @@ for dir in studentFolders:
         subprocess.run(f'java {execName}')
     
     os.chdir('..')
-
-
-
-'''
-        os.chdir(f'{studentName}')
-
-        subprocess.run('javac ' + programName)
-
-        os.startfile(f'{directory}\{studentName}\{programName}')
-
-        response = 'y'
-        while(response not in ['n', 'N', 'no', 'No']):
-            print(programName)
-
-            if(response in ['s', 'stack', 'S', 'Stack']):
-                subprocess.run(['java',
-                               programName[0: -5]],
-                               stdin=open(inStacked, 'r'))
-
-            elif response in ['y', 'Y', 'yes', 'Yes']:
-                subprocess.run(['java',
-                               programName[0: -5]])
-
-            else:
-                subprocess.run(['java',
-                               programName[0: -5]],
-                               stdin=open(inFile, 'r'))
-
-            response = input("Would you like to run the program again: ")
-
-        os.chdir(f'..')
-'''
