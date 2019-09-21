75ed5f0f43b40c2403edec0beb9389c9dd1915d8
Jordan <jordancottle622@gmail.com>
2019-09-21 17:18:24 -0400

    Seperate student files based on section

--- setup.py
+++ setup.py
@@ -6,26 +6,33 @@ import info
 ''' info.py requirements
 
     info.workingDirectory
-        - A string representating a path to a directory to place script output.
+        - A string representing a path to a directory to place script output.
         - Student folders will be created inside of this directory, with their programs located inside
 
     info.submissionDirectory
         - A string representing a path to the directory where the student submissions are located
-        - Simply unzip the bulk downlaod from canvas and use the extracted folder's path for this variable
+        - Simply unzip the bulk download from canvas and use the extracted folder's path for this variable
 '''
 
 pattern = re.compile(r'(\w+)_(?:\w*?_)?\d+_\d+_([\w\d-]+.java)')
 canvasRenamePattern = re.compile(r'(\w+)-\d+.java')
 
-workingDirectory = info.workingDirectory
+outputDirectory = info.outputDirectory
 
-if(not(os.path.exists(workingDirectory))):
-            os.mkdir(workingDirectory)
+if(not(os.path.exists(outputDirectory))):
+            os.mkdir(outputDirectory)
 
-os.chdir(workingDirectory)
+os.chdir(outputDirectory)
 
 directory = info.submissionDirectory
 files = os.listdir(directory)
+myStudents = set()
+notMyStudents = set()
+
+studentDict = info.studentList
+for section in studentDict:
+    for student in studentDict[section]:
+        myStudents.add(student)
 
 for fileName in files:
     print(fileName)
@@ -37,8 +44,17 @@ for fileName in files:
         if(programRenamed):
             programName = programRenamed.group(1) + '.java'
 
-        if(not(os.path.exists(studentName))):
-            os.mkdir(studentName)
+        if studentName not in myStudents:
+            outputFolder = 'others'
+        else:
+            if studentName in studentDict['003']:
+                outputFolder = '003'
+            else:
+                outputFolder = '009'
+
+        destination = f'{outputDirectory}/{outputFolder}/{studentName}'
+        if(not(os.path.exists(destination))):
+            os.makedirs(destination)
 
         shutil.copy(f'{directory}/{fileName}',
-                  f'{workingDirectory}/{studentName}/{programName}')
+                    f'{destination}/{fileName}')
