27347364a5cdd9926ba45e348f647b3b20d014fd
Jordan <jordancottle622@gmail.com>
2019-09-21 19:10:13 -0400

    organize students by section

--- grading.py
+++ grading.py
@@ -16,29 +16,37 @@ inputFiles = info.inputFiles
 inFile = inputFiles[0]
 inStacked = inputFiles[1]
 
-directory = info.workingDirectory
+directory = info.outputDirectory
 os.chdir(directory)
-studentFolders = os.listdir(directory)
 
-for dir in studentFolders:
-    print(dir)
-    os.chdir(dir)
+sections = os.listdir(directory)
+for section in sections:
+    if(section == 'others'):
+        continue
+    print(f'Section {section}')
+    os.chdir(section)
 
-    files = os.listdir('.')
+    studentFolders = os.listdir()
+    students = []
+    for dir in studentFolders:
+        print(dir)
+        os.chdir(dir)
+        files = os.listdir('.')
 
-    #remove .class files
-    for fileName in files:
-        if('.class' in fileName):
-            files.remove(fileName)
+        # remove .class files
+        for fileName in files:
+            if('.class' in fileName):
+                files.remove(fileName)
 
-    # compile files
-    for fileName in files:
-        print(fileName)
-        subprocess.run(f'javac {fileName}')
-    
-    # run files
-    for fileName in files:
-        execName = fileName.split('.')[0]
-        subprocess.run(f'java {execName}')
-    
+        # compile files
+        for fileName in files:
+            print(fileName)
+            subprocess.run(f'javac {fileName}')
+
+        # run files
+        for fileName in files:
+            execName = fileName.split('.')[0]
+            subprocess.run(f'java {execName}')
+
+        os.chdir('..')
     os.chdir('..')
